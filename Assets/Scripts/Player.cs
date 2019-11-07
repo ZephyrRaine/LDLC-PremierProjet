b2ccfec83401e7eec9d82f9624fa6f01ffdb2cd6
Colin <colin.le.duc@hotmail.com>
2019-11-07 11:15:32 +0100

    fait en cours

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //On récupère la position dans le monde
+        Vector3 positionVector = transform.position;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //On enlève -1 sur l'axe X de cette position
+            positionVector.x = positionVector.x - 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            positionVector.x = positionVector.x + 1;
+        }
+        //On donne au transform sa nouvelle position
+        transform.position = positionVector;
+    }
+}
