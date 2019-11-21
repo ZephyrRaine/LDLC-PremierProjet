e9e46730f3eb486738d582b6bf6ac486d2e07bc8
ZephyrRaine <zephyr.raine@gmail.com>
2019-11-21 10:20:19 +0100

    Direction player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,16 +15,40 @@ public class Player : MonoBehaviour
     {
         //On récupère la position dans le monde
         Vector3 positionVector = transform.position;
+        Vector3 direction = new Vector3(0,0,0); 
+
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             //On enlève -1 sur l'axe X de cette position
-            positionVector.x = positionVector.x - 1;
+            direction = new Vector3(-1, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            positionVector.x = positionVector.x + 1;
+            direction = new Vector3(1, 0, 0);
         }
-        //On donne au transform sa nouvelle position
-        transform.position = positionVector;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //On enlève -1 sur l'axe Y de cette position
+            direction = new Vector3(0, 1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = new Vector3(0, -1, 0);
+        }
+
+        if (Physics2D.Raycast(transform.position, direction, 1f, LayerMask.GetMask("Wall")))
+        {
+            //Il y a un mur à 1 mètre ou moins à partir de "vectorPosition", et en direction de "vectorDirection"
+        }
+        else
+        {
+            //Si il n'y a pas de mur
+            //On donne au transform sa nouvelle position
+            //transform.position = transform.position + direction;
+            transform.position += direction;
+        }
+
     }
 }
